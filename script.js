46c97c6d4e19ea2a4863b678a6cd5c01ef6ab76b
sameerpujari1981 <167015005+sameerpujari1981@users.noreply.github.com>
2024-04-22 10:44:37 +0530

    receptcha validation added

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function beforesubmit(){
-    let outputdate = document.querySelector(".outputdate");
-    let inputdate = document.querySelector(".inputdate");
-    console.log("Input date value =>",inputdate.value);
+let captchChecked = false;
+function beforesubmit(event){
+    if(captchChecked){
+        let outputdate = document.querySelector(".outputdate");
+        let inputdate = document.querySelector(".inputdate");
+        console.log("Input date value =>",inputdate.value);
 
-    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
-    console.log("formattedDate =>",formattedDate);
-    outputdate.value = formattedDate;
-    console.log("outputdate.value =>",outputdate.value);
+        let formattedDate = new Date(inputdate.value).toLocaleDateString("en-IN");
+        console.log("formattedDate =>",formattedDate);
+        outputdate.value = formattedDate;
+        console.log("outputdate.value =>",outputdate.value);
+    } else {
+        alert ("Please check reCAPTCH box to submit the lead.");
+        event.preventdefault();
+    }    
 }
 
-function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+function timestamp() { 
+    var response = document.getElementById("g-recaptcha-response"); 
+    if (response == null || response.value.trim() == "") 
+    {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500); 
+
+function captchasuccess(){
+    captchChecked = true;
+}
